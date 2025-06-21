a6695b2f37cb5f9e0aa21f93e859008be5f68244
leye195 <leye195@naver.com>
2025-06-22 01:30:28 +0900

    feat: add mobile header

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -50,7 +50,7 @@ export default async function RootLayout({
         <NextIntlClientProvider messages={messages}>
           <ReactQueryProvider>
             <Header />
-            <main className="min-h-[calc(100vh-200px)] bg-white">
+            <main className="mx-auto min-h-[calc(100vh-200px)] max-w-[1920px] bg-white">
               {children}
             </main>
             <Footer />
--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,61 +1,139 @@
+import { MenuIcon } from "lucide-react";
 import Image from "next/image";
 import { Link } from "@/i18n/navigation";
 import { cn } from "@/shared/lib/style";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/shared/ui/sheet";
+
+const styleMap = {
+  deskTop: {
+    menu: "py-[20px]",
+  },
+  mobile: {
+    menu: "flex h-[40px] items-center px-[20px]",
+  },
+};
+
+function Desktop() {
+  return (
+    <div
+      className={cn(
+        "mx-auto flex h-full w-full max-w-[1920px] items-center justify-between",
+        "max-md:hidden",
+      )}
+    >
+      <div className="flex items-center gap-[40px]">
+        <Link className="h-full" href="/">
+          <Image
+            alt=""
+            className="h-full"
+            height={80}
+            src="/logo.png"
+            width={120}
+          />
+        </Link>
+        <ul className="flex gap-[40px]">
+          <li>
+            <Link className={styleMap.deskTop.menu} href="/product">
+              Product
+            </Link>
+          </li>
+          <li>
+            <Link className={styleMap.deskTop.menu} href="/brand">
+              Brand
+            </Link>
+          </li>
+          <li>
+            <Link className={styleMap.deskTop.menu} href="/news">
+              News
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <ul className="flex items-center gap-[40px]">
+          <li>
+            <Link className={styleMap.deskTop.menu} href="/about">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link className={styleMap.deskTop.menu} href="/contact">
+              Contact
+            </Link>
+          </li>
+          <li>KR</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+function Mobile() {
+  return (
+    <div className={cn("hidden", "max-md:flex")}>
+      <Sheet>
+        <SheetTrigger asChild>
+          <button className="cursor-pointer" type="button">
+            <MenuIcon />
+          </button>
+        </SheetTrigger>
+        <SheetContent hideClose side="left">
+          <SheetHeader>
+            <SheetTitle className="sr-only" />
+            <SheetDescription className="sr-only" />
+          </SheetHeader>
+          <ul className="flex flex-col">
+            <li>
+              <Link className={styleMap.mobile.menu} href="/product">
+                Product
+              </Link>
+            </li>
+            <li>
+              <Link className={styleMap.mobile.menu} href="/brand">
+                Brand
+              </Link>
+            </li>
+            <li>
+              <Link className={styleMap.mobile.menu} href="/news">
+                News
+              </Link>
+            </li>
+            <li>
+              <Link className={styleMap.mobile.menu} href="/about">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link className={styleMap.mobile.menu} href="/contact">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </SheetContent>
+      </Sheet>
+    </div>
+  );
+}
 
 export function Header() {
   return (
     <header
       className={cn(
-        "h-[56px] w-full bg-white px-[40px]",
+        "h-[60px] w-full bg-white px-[20px]",
         "fixed top-0 left-0",
         "flex items-center text-[20px]",
+        "max-md:h-[50px]",
       )}
     >
-      <div className="flex h-full w-full items-center justify-between">
-        <div className="flex items-center gap-[40px]">
-          <Link className="h-full" href="/">
-            <Image
-              alt=""
-              className="h-full"
-              height={80}
-              src="/logo.png"
-              width={120}
-            />
-          </Link>
-          <ul className="flex gap-[40px]">
-            <li>
-              <Link className="py-[20px]" href="/product">
-                Product
-              </Link>
-            </li>
-            <li>
-              <Link className="py-[20px]" href="/brand">
-                Brand
-              </Link>
-            </li>
-            <li>
-              <Link className="py-[20px]" href="/news">
-                News
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div>
-          <ul className="flex items-center gap-[40px]">
-            <li>
-              <Link className="py-[20px]" href="/about">
-                About
-              </Link>
-            </li>
-            <li>
-              <Link className="py-[20px]" href="/contact">
-                Contact
-              </Link>
-            </li>
-            <li>KR</li>
-          </ul>
-        </div>
-      </div>
+      <Desktop />
+      <Mobile />
     </header>
   );
 }
