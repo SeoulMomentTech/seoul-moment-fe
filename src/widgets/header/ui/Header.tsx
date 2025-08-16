312a43bcb0fd37c11de4e66152ca35a5aaf1ca48
leye195 <leye195@naver.com>
2025-08-16 16:13:16 +0900

    feat: update header logo & ui

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,4 +1,4 @@
-import { MenuIcon } from "lucide-react";
+import { MenuIcon, ShoppingCartIcon, ChevronRightIcon } from "lucide-react";
 import Image from "next/image";
 import { Link } from "@/i18n/navigation";
 import { cn } from "@/shared/lib/style";
@@ -13,10 +13,10 @@ import {
 
 const styleMap = {
   deskTop: {
-    menu: "py-[20px]",
+    menu: "text-[14px] pt-[20px] pb-[20px] transition-all hover:border-b hover:border-b-black hover:font-semibold",
   },
   mobile: {
-    menu: "flex h-[40px] items-center px-[20px]",
+    menu: "text-[14px] flex justify-between h-[42px] items-center",
   },
 };
 
@@ -33,9 +33,9 @@ function Desktop() {
           <Image
             alt=""
             className="h-full"
-            height={80}
+            height={24}
             src="/logo.png"
-            width={120}
+            width={200}
           />
         </Link>
         <ul className="flex gap-[40px]">
@@ -68,7 +68,7 @@ function Desktop() {
               Contact
             </Link>
           </li>
-          <li>KR</li>
+          <li className="h-[56px] py-[20px] text-[14px]">language</li>
         </ul>
       </div>
     </div>
@@ -77,62 +77,67 @@ function Desktop() {
 
 function Mobile() {
   return (
-    <div className={cn("hidden", "relative h-full w-full max-md:flex")}>
-      <Sheet>
-        <SheetTrigger asChild>
-          <button className="cursor-pointer" type="button">
-            <MenuIcon />
-          </button>
-        </SheetTrigger>
-        <SheetContent hideClose side="left">
-          <SheetHeader>
-            <SheetTitle className="sr-only" />
-            <SheetDescription className="sr-only" />
-          </SheetHeader>
-          <ul className="flex flex-col">
-            <li>
-              <Link className={styleMap.mobile.menu} href="/product">
-                Product
-              </Link>
-            </li>
-            <li>
-              <Link className={styleMap.mobile.menu} href="/brand">
-                Brand
-              </Link>
-            </li>
-            <li>
-              <Link className={styleMap.mobile.menu} href="/news">
-                News
-              </Link>
-            </li>
-            <li>
-              <Link className={styleMap.mobile.menu} href="/about">
-                About
-              </Link>
-            </li>
-            <li>
-              <Link className={styleMap.mobile.menu} href="/contact">
-                Contact
-              </Link>
-            </li>
-          </ul>
-        </SheetContent>
-      </Sheet>
-      <Link
-        className={cn(
-          "absolute top-0 h-[60px] w-[120px]",
-          "left-[50%] translate-x-[-50%]",
-        )}
-        href="/"
-      >
-        <Image
-          alt=""
-          className="h-full"
-          height={80}
-          src="/logo.png"
-          width={120}
-        />
-      </Link>
+    <div
+      className={cn(
+        "hidden",
+        "h-full w-full max-md:flex max-md:items-center max-md:justify-between",
+      )}
+    >
+      <div className="flex items-center gap-[10px]">
+        <Sheet>
+          <SheetTrigger asChild>
+            <button className="cursor-pointer" type="button">
+              <MenuIcon />
+            </button>
+          </SheetTrigger>
+          <SheetContent
+            className="flex w-full px-[20px] sm:max-w-screen"
+            side="left"
+          >
+            <SheetHeader className="ml-[34px] flex px-0">
+              <Image alt="" height={16} src="/logo.png" width={133} />
+              <SheetTitle className="sr-only" />
+              <SheetDescription className="sr-only" />
+            </SheetHeader>
+            <ul className="flex flex-col">
+              <li>
+                <Link className={styleMap.mobile.menu} href="/product">
+                  Product
+                  <ChevronRightIcon height={16} width={16} />
+                </Link>
+              </li>
+              <li>
+                <Link className={styleMap.mobile.menu} href="/brand">
+                  Brand
+                  <ChevronRightIcon height={16} width={16} />
+                </Link>
+              </li>
+              <li>
+                <Link className={styleMap.mobile.menu} href="/news">
+                  News
+                  <ChevronRightIcon height={16} width={16} />
+                </Link>
+              </li>
+              <li>
+                <Link className={styleMap.mobile.menu} href="/about">
+                  About
+                  <ChevronRightIcon height={16} width={16} />
+                </Link>
+              </li>
+              <li>
+                <Link className={styleMap.mobile.menu} href="/contact">
+                  Contact
+                  <ChevronRightIcon height={16} width={16} />
+                </Link>
+              </li>
+            </ul>
+          </SheetContent>
+        </Sheet>
+        <Link className={cn()} href="/">
+          <Image alt="" height={16} src="/logo.png" width={133} />
+        </Link>
+      </div>
+      <ShoppingCartIcon />
     </div>
   );
 }
@@ -141,9 +146,10 @@ export function Header() {
   return (
     <header
       className={cn(
-        "h-[60px] w-full bg-white px-[20px]",
+        "h-[56px] w-full bg-white px-[20px]",
+        "border-b border-b-white/5",
         "fixed top-0 left-0",
-        "flex items-center text-[20px]",
+        "flex items-center",
       )}
     >
       <Desktop />
