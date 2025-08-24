e08be0ec19a5c4903aa3504049d7a879a0e81752
leye195 <leye195@naver.com>
2025-08-24 19:26:08 +0900

    feat: add widgets/card - component

---
+++ src/widgets/card/ui/Card.tsx
@@ -0,0 +1,31 @@
+import type { ReactNode } from "react";
+import { cn } from "@/shared/lib/style";
+
+interface CardProps {
+  image: ReactNode;
+  title: string | ReactNode;
+  subTitle: string | ReactNode;
+  extraInfo: string | ReactNode;
+  className?: string;
+}
+
+export function Card({
+  className,
+  image,
+  title,
+  subTitle,
+  extraInfo,
+}: CardProps) {
+  return (
+    <div className={cn("flex flex-col gap-[30px]", className)}>
+      {image}
+      <div className="flex flex-col gap-[30px]">
+        <div className="flex flex-col gap-[10px]">
+          {title}
+          {subTitle}
+        </div>
+        {extraInfo}
+      </div>
+    </div>
+  );
+}
