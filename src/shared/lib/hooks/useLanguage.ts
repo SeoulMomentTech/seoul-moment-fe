961754c6f0f43d08fc6c1629fa7adaaa8b785db7
leye195 <leye195@naver.com>
2025-09-19 00:17:43 +0900

    feat: add useLanguage hook

---
+++ src/shared/lib/hooks/useLanguage.ts
@@ -0,0 +1,13 @@
+"use client";
+
+import { useParams } from "next/navigation";
+import type { LanguageType } from "@/i18n/const";
+
+const useLanguage = () => {
+  const params = useParams();
+  const lang = params.locale as LanguageType;
+
+  return lang ?? "ko";
+};
+
+export default useLanguage;
