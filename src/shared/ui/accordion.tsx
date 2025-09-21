818100150fcfb38b0fe805bedd182c364324d9d3
leye195 <leye195@naver.com>
2025-09-21 23:35:54 +0900

    feat: add accordion component

--- src/shared/ui/accordion.tsx
+++ src/shared/ui/accordion.tsx
@@ -10,13 +10,20 @@ function Accordion({
 }: React.ComponentProps<typeof AccordionPrimitive.Root>) {
   return <AccordionPrimitive.Root data-slot="accordion" {...props} />;
 }
+
+interface AccordionItemInterface
+  extends React.ComponentProps<typeof AccordionPrimitive.Item> {
+  hideBorder?: boolean;
+}
+
 function AccordionItem({
   className,
+  hideBorder,
   ...props
-}: React.ComponentProps<typeof AccordionPrimitive.Item>) {
+}: AccordionItemInterface) {
   return (
     <AccordionPrimitive.Item
-      className={cn("border-b last:border-b-0", className)}
+      className={cn(!hideBorder && "border-b last:border-b-0", className)}
       data-slot="accordion-item"
       {...props}
     />
@@ -31,7 +38,7 @@ function AccordionTrigger({
     <AccordionPrimitive.Header className="flex">
       <AccordionPrimitive.Trigger
         className={cn(
-          "focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-start justify-between gap-4 rounded-md py-4 text-left text-sm font-medium transition-all outline-none hover:underline focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&[data-state=open]>svg]:rotate-180",
+          "focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-start justify-between gap-4 rounded-md py-4 text-left text-sm font-medium transition-all outline-none hover:cursor-pointer focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&[data-state=open]>svg]:rotate-180",
           className,
         )}
         data-slot="accordion-trigger"
