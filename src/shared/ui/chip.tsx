f8ae254128474cbaba9ace4b236cb72416366320
leye195 <leye195@naver.com>
2025-10-20 23:25:25 +0900

    feat: share/ui add chip component

---
+++ src/shared/ui/chip.tsx
@@ -0,0 +1,31 @@
+import type { ComponentProps } from "react";
+import { Button } from "./button";
+import { cn } from "../lib/style";
+
+interface ChipProps extends ComponentProps<"button"> {
+  className?: string;
+  active?: boolean;
+}
+
+export default function Chip({
+  active,
+  className,
+  children,
+  ...props
+}: ChipProps) {
+  return (
+    <Button
+      className={cn(
+        "flex items-center justify-center rounded-full px-[12px] py-[8px]",
+        "text-body-3 border border-black/10 bg-white",
+        active &&
+          "border-black bg-black font-semibold text-white hover:bg-black",
+        className,
+      )}
+      {...props}
+      variant="outline"
+    >
+      {children}
+    </Button>
+  );
+}
