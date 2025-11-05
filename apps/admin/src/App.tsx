16d90b70b645bcd470afbf98ef4f9bb3a9f21ed0
leye195 <leye195@naver.com>
2025-11-05 23:06:45 +0900

    chore: integrate Tailwind CSS configuration and update package dependencies
    - Added a new @seoul-moment/tailwind-config package for centralized Tailwind CSS styles and configurations.
    - Updated package.json files across apps to include Tailwind CSS dependencies.
    - Refactored PostCSS configurations in apps to utilize the new Tailwind setup.
    - Removed unused App.css file from the admin app for cleaner structure.

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -1,13 +1,12 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="mx-auto flex min-h-screen flex-col items-center justify-center text-center">
       <div>
         <a href="https://vite.dev" target="_blank">
           <img alt="Vite logo" className="logo" src={viteLogo} />
@@ -16,9 +15,13 @@ function App() {
           <img alt="React logo" className="logo react" src={reactLogo} />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="">Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)} type="button">
+        <button
+          className="text-body-1 rounded-full border p-2 font-semibold"
+          onClick={() => setCount((count) => count + 1)}
+          type="button"
+        >
           count is {count}
         </button>
         <p>
@@ -28,7 +31,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   );
 }
 
