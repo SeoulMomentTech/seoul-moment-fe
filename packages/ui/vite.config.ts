f497dd673c20eb2824c29a0ada85e219ceadd1f2
leye195 <leye195@naver.com>
2025-11-16 01:23:03 +0900

    refactor: adjust import order

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,6 +1,8 @@
 import { resolve } from "node:path";
-import react from "@vitejs/plugin-react";
+
 import { defineConfig } from "vite";
+
+import react from "@vitejs/plugin-react";
 
 const entry = resolve(__dirname, "src/index.ts");
 
