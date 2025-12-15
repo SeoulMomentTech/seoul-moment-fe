a5f2dcc2e31db715f062d36a8cfe575b405f25f6
leye195 <leye195@naver.com>
2025-12-16 00:38:20 +0900

    feat(web): apply no-restricted-imports rule

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -1,5 +1,44 @@
 import nextJsConfig from "@seoul-moment/eslint-config/next-js";
 
-const config = [...nextJsConfig];
+const config = [
+  {
+    rules: {
+      "no-restricted-imports": [
+        "error",
+        {
+          paths: [
+            {
+              name: "@tanstack/react-query",
+              importNames: ["useQuery"],
+              message: "useAppQuery를 사용하세요.",
+            },
+            {
+              name: "@tanstack/react-query",
+              importNames: ["useInfiniteQuery"],
+              message: "useAppInfiniteQuery를 사용하세요.",
+            },
+            {
+              name: "@tanstack/react-query",
+              importNames: ["useMutation"],
+              message: "useAppMutation을 사용하세요.",
+            },
+          ],
+        },
+      ],
+    },
+  },
+  {
+    files: [
+      "**/useAppQuery.ts",
+      "**/useAppInfiniteQuery.ts",
+      "**/useAppMutation.ts",
+    ],
+    rules: {
+      "no-restricted-imports": "off",
+    },
+  },
+
+  ...nextJsConfig,
+];
 
 export default config;
