12bc21e7ba1576c4dc528bad9f4ebe00f8fe389a
leye195 <leye195@naver.com>
2025-12-27 23:34:55 +0900

    feat: define product subCategory route path

---
+++ apps/admin/src/pages/ProductSubCategoriesPage/index.tsx
@@ -0,0 +1,9 @@
+import { SubCategoryHeader } from "./components/SubCategoryHeader";
+
+export default function ProductSubCategoriesPage() {
+  return (
+    <div className="p-8 pt-24">
+      <SubCategoryHeader onClickAdd={() => {}} />
+    </div>
+  );
+}
