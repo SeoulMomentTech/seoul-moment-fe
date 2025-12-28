fb7ffd1824955c54db48c6a61adbd9d6223c693b
leye195 <leye195@naver.com>
2025-12-28 18:15:12 +0900

    feat: define product list api & query hooks

---
+++ apps/admin/src/shared/services/products.ts
@@ -0,0 +1,45 @@
+import { fetcher } from ".";
+
+interface ApiResponse<T> {
+  result: boolean;
+  data: T;
+}
+
+export type AdminProductItemSort = "ASC" | "DESC";
+
+export type AdminProductItemId = Branded<number, "AdminProductItemId">;
+
+export type AdminProductId = Branded<number, "AdminProductId">;
+
+export interface AdminProductItem {
+  id: AdminProductItemId;
+  productId: AdminProductId;
+  imageUrl: string;
+  colorCode: string;
+  price: number;
+  discountPrice: number;
+  createDate: string;
+  updateDate: string;
+}
+
+interface AdminProductItemListData {
+  total: number;
+  list: AdminProductItem[];
+}
+
+export interface AdminProductItemListParams {
+  page?: number;
+  count?: number;
+  search?: string;
+  sort?: AdminProductItemSort;
+}
+
+/**
+ * @description 상품 정보 리스트
+ */
+export const getAdminProductItemList = (
+  params?: AdminProductItemListParams,
+) =>
+  fetcher.get<ApiResponse<AdminProductItemListData>>("/admin/product/item", {
+    params,
+  });
