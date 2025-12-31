a914f3348eac1e803b28ef99a8f5f5bb5b378dd1
leye195 <leye195@naver.com>
2025-12-31 22:44:54 +0900

    feat: add useAdminBrandSuspenseQuery

--- apps/admin/src/pages/BrandPage/hooks/useAdminBrandQuery.ts
+++ apps/admin/src/pages/BrandPage/hooks/useAdminBrandQuery.ts
@@ -1,6 +1,11 @@
 import { getAdminBrandInfo, type BrandId } from "@shared/services/brand";
 
-import { useQuery, type UseQueryOptions } from "@tanstack/react-query";
+import {
+  useQuery,
+  useSuspenseQuery,
+  type UseQueryOptions,
+  type UseSuspenseQueryOptions,
+} from "@tanstack/react-query";
 
 import { brandQueryKeys } from "./queryKeys";
 
@@ -8,6 +13,16 @@ type AdminBrandQueryResponse = Awaited<ReturnType<typeof getAdminBrandInfo>>;
 
 type AdminBrandQueryOptions = Omit<
   UseQueryOptions<
+    AdminBrandQueryResponse,
+    unknown,
+    AdminBrandQueryResponse,
+    ReturnType<typeof brandQueryKeys.detail>
+  >,
+  "queryKey" | "queryFn"
+>;
+
+type AdminBrandSuspenseQueryOptions = Omit<
+  UseSuspenseQueryOptions<
     AdminBrandQueryResponse,
     unknown,
     AdminBrandQueryResponse,
@@ -25,4 +40,14 @@ export const useAdminBrandQuery = (
     queryFn: () => getAdminBrandInfo(brandId),
     ...options,
     enabled: options?.enabled ?? Boolean(brandId),
+  });
+
+export const useAdminBrandSuspenseQuery = (
+  brandId: BrandId,
+  options?: AdminBrandSuspenseQueryOptions,
+) =>
+  useSuspenseQuery({
+    queryKey: brandQueryKeys.detail(brandId),
+    queryFn: () => getAdminBrandInfo(brandId),
+    ...options,
   });
