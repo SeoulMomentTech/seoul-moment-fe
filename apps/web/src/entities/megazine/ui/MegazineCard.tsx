46d70c9424c5438ecb1756e62f87d78318b852ef
leye195 <leye195@naver.com>
2026-01-29 21:50:52 +0900

    style: adjust width on MegazineCard

--- apps/web/src/entities/megazine/ui/MegazineCard.tsx
+++ apps/web/src/entities/megazine/ui/MegazineCard.tsx
@@ -1,6 +1,8 @@
 import { BaseImage } from "@shared/ui/base-image";
 import { Card } from "@shared/ui/card";
 
+import { cn } from "@seoul-moment/ui";
+
 interface MegazineCardProps {
   title: string;
   imageUrl: string;
@@ -9,17 +11,25 @@ interface MegazineCardProps {
 export function MegazineCard({ title, imageUrl }: MegazineCardProps) {
   return (
     <Card
-      className="gap-[20px]"
+      className={cn("w-[400px] gap-[20px]", "max-sm:w-auto")}
       image={
         <BaseImage
           alt=""
-          className="h-[260px] w-[400px] bg-slate-300 max-sm:h-[210px] max-sm:w-full"
+          className={cn(
+            "h-[260px] bg-slate-300",
+            "max-sm:h-[210px] max-sm:w-full",
+          )}
           height={260}
           src={imageUrl}
           width={400}
         />
       }
-      title={<span className="max-sm:font-semibold">{title}</span>}
+      title={
+        <span className="max-sm:font-semibold">
+          {title}
+          {title}
+        </span>
+      }
     />
   );
 }
