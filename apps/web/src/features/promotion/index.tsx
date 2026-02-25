5d079bae6336d18f97ad73b6de72b15a8503ac7f
leye195 <leye195@naver.com>
2026-02-26 01:00:06 +0900

    feat: add BrandOnlineEvent

--- apps/web/src/features/promotion/index.tsx
+++ apps/web/src/features/promotion/index.tsx
@@ -4,3 +4,4 @@ export { BrandIntroduction } from "./ui/BrandIntroduction";
 export { BrandLookbook } from "./ui/BrandLookbook";
 export { BrandSpecialEvent } from "./ui/BrandSpecialEvent";
 export { BrandOfflinePopup } from "./ui/BrandOfflinePopup";
+export { BrandOnlineEvent } from "./ui/BrandOnlineEvent";
