b3360f60800e6da39b58e31eaf54b63e20cab2ab
leye195 <leye195@naver.com>
2026-02-26 01:00:06 +0900

    feat: add BrandOnlineEvent

--- apps/web/src/views/promotion/ui/PromotionPage.tsx
+++ apps/web/src/views/promotion/ui/PromotionPage.tsx
@@ -9,6 +9,7 @@ import {
   BrandLookbook,
   BrandSpecialEvent,
   BrandOfflinePopup,
+  BrandOnlineEvent,
 } from "@/features/promotion";
 
 export default function PromotionPage() {
@@ -22,6 +23,7 @@ export default function PromotionPage() {
       <BrandLookbook />
       <BrandSpecialEvent />
       <BrandOfflinePopup />
+      <BrandOnlineEvent />
     </>
   );
 }
